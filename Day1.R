4bd4f779dae98a8fd10b5fa97c399e6b3931c1b9
Ferdi <128728342+Ferdinand23457@users.noreply.github.com>
2024-12-02 17:10:24 +0100

    Removed local file path

--- Day1.R
+++ Day1.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-table <- read.table("C:\\Users\\ferdi\\Downloads\\input.txt", header = FALSE, sep= "")
+table <- read.table("your_path_here", header = FALSE, sep= "")
 table$V1 <- sort(table$V1)
 table$V2 <- sort(table$V2)
 table$diff <-table$V1 - table$V2
